b885ef6637ac52810464b7822d0e6e5f485a0c71
Nikita Volobuev <nikitavbv@gmail.com>
2020-06-17 23:48:38 +0300

    feat(proxy): simple http server with hyper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+use hyper::{Request, Body, Response, Server};
+
+#[tokio::main]
+async fn main() {
+    let addr = ([127, 0, 0, 1], 8080).into();
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(proxy_service))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+async fn proxy_service(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    println!("host is {}", req.headers().get("Host").unwrap().to_str().unwrap());
+    Ok(Response::new("hello".into()))
 }
