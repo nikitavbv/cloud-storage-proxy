be8cdb372f22d0d4b81684113e755f37814c055a
Nikita Volobuev <nikitavbv@gmail.com>
2020-08-01 23:51:31 +0300

    feat(caching): write to existing cache

--- src/main.rs
+++ src/main.rs
@@ -108,7 +108,6 @@ async fn proxy_service(
 
     let obj_cache = cache.lock().await;
     //let object = obj_cache.get(&object_name).await;
-    let cache_collection = cache_collection.clone();
 
     /*let object = match object {
         Some(v) => {
