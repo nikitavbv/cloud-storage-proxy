9c74fcb7ba29ae14d6650243d64ba9aea9e7d7d4
Nikita Volobuev <nikitavbv@gmail.com>
2020-11-21 23:51:55 +0200

    feat(rate_limiting): add metrics

--- src/rate_limiting/local.rs
+++ src/rate_limiting/local.rs
@@ -2,6 +2,17 @@ use std::collections::HashMap;
 use actix::{Actor, Context, Handler};
 use crate::rate_limiting::messages::{PutRateLimitingStats, RateLimitingEntry, RateLimitingError};
 
+lazy_static! {
+    static ref LOCAL_RATE_LIMITED: Gauge = register_gauge!(
+        "local_rate_limiter_limited",
+        "local rate limiter limited requests"
+    ).unwrap();
+    static ref LOCAL_OK: Counter = register_counter!(
+        "local_rate_limiter_ok",
+        "local rate limiter ok requests"
+    ).unwrap();
+}
+
 pub struct LocalRateLimiter {
     stats: HashMap<String, (u64, u64)>,
 }
